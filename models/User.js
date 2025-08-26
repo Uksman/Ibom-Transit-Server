1688ecc80a59ec0ba6ddc36e2e5fb0ff9b0834a8
Uksman <101516047+Uksman@users.noreply.github.com>
2025-08-26 03:51:58 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -102,7 +102,7 @@ UserSchema.pre('save', async function(next) {
     next();
   }
 
-  const salt = await bcrypt.genSalt(10);
+  const salt = await bcrypt.genSalt(8);
   this.password = await bcrypt.hash(this.password, salt);
   next();
 });
